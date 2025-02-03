8ad53b48663a47c50cd222acbe3343075a899ef9
Benjamin Hall <benjaminjahall@gmail.com>
2025-02-03 03:00:14 -0500

    setup project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { CubeApi, type Query } from "@cubejs-client/core";
+
 type Parser = (input: unknown) => unknown;
 type Keys = Record<string, Parser>;
 
@@ -7,24 +9,18 @@ type Cube<Measures extends Keys, Dimensions extends Keys> = {
   dimensions: Dimensions;
 };
 
-function elideCube<Measures extends Keys, Dimensions extends Keys>(def: {
-  name: string;
-  measures: Measures;
-  dimensions: Dimensions;
-}): Cube<Measures, Dimensions> {
+export function elideCube<Measures extends Keys, Dimensions extends Keys>(def: Cube<Measures, Dimensions>): Cube<Measures, Dimensions> {
   return def;
 }
-
-type ExtendsCube<T> = T extends Cube<infer M, infer D> ? Cube<M, D> : never;
 
 type ResponseBuilder<
   Measures extends Keys,
   Dimensions extends Keys,
   InputMeasures extends keyof Measures,
-  InputDimensions extends keyof Dimensions
+  InputDimensions extends keyof Dimensions,
 > = {
   measure<Name extends keyof Measures>(
-    name: Name
+    name: Name,
   ): ResponseBuilder<
     Measures,
     Dimensions,
@@ -32,21 +28,22 @@ type ResponseBuilder<
     InputDimensions
   >;
   dimension<Name extends keyof Dimensions>(
-    name: Name
+    name: Name,
   ): ResponseBuilder<
     Measures,
     Dimensions,
     InputMeasures,
     InputDimensions | Name
   >;
-  finalize(): {
-    measures: { [K in InputMeasures]: Measures[K] };
-    dimensions: { [K in InputDimensions]: Dimensions[K] };
+  load(api: CubeApi): {
+    [K in InputMeasures]: Measures[K];
+  } & {
+    [K in InputDimensions]: Dimensions[K];
   };
 };
 
-function consume<Measures extends Keys, Dimensions extends Keys>(
-  cube: Cube<Measures, Dimensions>
+export function consume<Measures extends Keys, Dimensions extends Keys>(
+  cube: Cube<Measures, Dimensions>,
 ): ResponseBuilder<Measures, Dimensions, never, never> {
   return responseBuilder(cube, [], []);
 }
@@ -55,11 +52,11 @@ function responseBuilder<
   Measures extends Keys,
   Dimensions extends Keys,
   InputMeasures extends keyof Measures,
-  InputDimensions extends keyof Dimensions
+  InputDimensions extends keyof Dimensions,
 >(
   cube: Cube<Measures, Dimensions>,
   measures: InputMeasures[],
-  dimensions: InputDimensions[]
+  dimensions: InputDimensions[],
 ): ResponseBuilder<Measures, Dimensions, InputMeasures, InputDimensions> {
   return {
     measure: <Name extends keyof Measures>(name: Name) => {
@@ -68,23 +65,39 @@ function responseBuilder<
     dimension: <Name extends keyof Dimensions>(name: Name) => {
       return responseBuilder(cube, measures, [...dimensions, name]);
     },
-    finalize: () => {
+    load: (api) => {
+      const query: Query = {};
+      if (measures.length) {
+        query.measures = measures.map((ms) => `${cube.name}.${String(ms)}`);
+      }
+      if (dimensions.length) {
+        query.dimensions = dimensions.map((dm) => `${cube.name}.${String(dm)}`);
+      }
+
       return {
-        measures: measures.reduce((acc, curr) => {
-          acc[curr] = cube.measures[curr];
-          return acc;
-        }, {} as { [K in InputMeasures]: Measures[K] }),
-        dimensions: dimensions.reduce((acc, curr) => {
-          acc[curr] = cube.dimensions[curr];
-          return acc;
-        }, {} as { [K in InputDimensions]: Dimensions[K] }),
+        ...measures.reduce(
+          (acc, curr) => {
+            acc[curr] = cube.measures[curr];
+            return acc;
+          },
+          {} as { [K in InputMeasures]: Measures[K] },
+        ),
+        ...dimensions.reduce(
+          (acc, curr) => {
+            acc[curr] = cube.dimensions[curr];
+            return acc;
+          },
+          {} as { [K in InputDimensions]: Dimensions[K] },
+        ),
       };
     },
   };
 }
 
-async function test() {
-  const cubeDef = {
+async function testCube() {
+  const api = new CubeApi();
+
+  const cubeDef = elideCube({
     name: "test",
     measures: {
       myMeasure: Number,
@@ -94,11 +107,11 @@ async function test() {
     dimensions: {
       myDimension: String,
     },
-  };
+  });
 
   const cubeResponse = consume(cubeDef)
     .measure("myMeasure")
     .measure("myOther")
     .dimension("myDimension")
-    .finalize();
+    .load(api);
 }
