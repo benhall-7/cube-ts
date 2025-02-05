0376d1da4d0cfe6e96c77aecc56d6ac0e2d493f1
Benjamin Hall <benjaminjahall@gmail.com>
2025-02-05 17:26:48 -0500

    Rework to use class

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,142 @@
-import { CubeApi, type Query } from "@cubejs-client/core";
+import {
+  BinaryOperator,
+  CubeApi,
+  UnaryOperator,
+  type Query,
+} from "@cubejs-client/core";
 
-type Parser = (input: unknown) => unknown;
-type Keys = Record<string, Parser>;
+type MemberType = "string" | "time" | "boolean" | "number";
+type Member = {
+  type: MemberType;
+};
+type Keys = Record<string, Member>;
 
-type Cube<Measures extends Keys, Dimensions extends Keys> = {
+class CubeDef<Measures extends Keys, Dimensions extends Keys> {
   name: string;
   measures: Measures;
   dimensions: Dimensions;
-};
 
-export function elideCube<Measures extends Keys, Dimensions extends Keys>(def: Cube<Measures, Dimensions>): Cube<Measures, Dimensions> {
-  return def;
+  constructor(name: string, measures: Measures, dimensions: Dimensions) {
+    this.name = name;
+    this.measures = measures;
+    this.dimensions = dimensions;
+  }
+
+  buildQuery(): QueryBuilder<Measures, Dimensions, never, never> {
+    return responseBuilder(this, [], []);
+  }
+
+  measure<Name extends keyof Measures>(name: Name): string {
+    return `${this.name}.${String(name)}`
+  }
+
+  dimension<Name extends keyof Dimensions>(name: Name): string {
+    return `${this.name}.${String(name)}`
+  }
 }
 
-type ResponseBuilder<
+/*
+
+  export type Filter = BinaryFilter | UnaryFilter | LogicalOrFilter | LogicalAndFilter;
+  export type LogicalAndFilter = {
+    and: Filter[];
+  };
+
+  export type LogicalOrFilter = {
+    or: Filter[];
+  };
+
+  export interface BinaryFilter {
+    dimension?: string;
+    member?: string;
+    operator: BinaryOperator;
+    values: string[];
+  }
+  export interface UnaryFilter {
+    dimension?: string;
+    member?: string;
+    operator: UnaryOperator;
+    values?: never;
+  }
+  export type UnaryOperator = 'set' | 'notSet';
+  export type BinaryOperator =
+    | 'equals'
+    | 'notEquals'
+    | 'contains'
+    | 'notContains'
+    | 'startsWith'
+    | 'notStartsWith'
+    | 'endsWith'
+    | 'notEndsWith'
+    | 'gt'
+    | 'gte'
+    | 'lt'
+    | 'lte'
+    | 'inDateRange'
+    | 'notInDateRange'
+    | 'beforeDate'
+    | 'beforeOrOnDate'
+    | 'afterDate'
+    | 'afterOrOnDate';
+*/
+
+type Filter<Measures extends string, Dimensions extends string> =
+  | UnaryFilter<Measures>
+  | UnaryFilter<Dimensions>
+  | BinaryFilter<Measures>
+  | BinaryFilter<Dimensions>
+  // can't mix-and-match measures and dimensions in these filter
+  // types, so we use never to exclude them recursively
+  | LogicalAndFilter<Measures, never>
+  | LogicalAndFilter<never, Dimensions>
+  | LogicalOrFilter<Measures, never>
+  | LogicalOrFilter<never, Dimensions>;
+// TODO: restrict values to the correct type
+type BinaryFilter<Member extends string> = {
+  member: Member;
+  operator: BinaryOperator;
+  values: string[];
+};
+
+type UnaryFilter<Member extends string> = {
+  member: Member;
+  operator: UnaryOperator;
+  values?: never;
+};
+
+type LogicalAndFilter<Measures extends string, Dimensions extends string> = {
+  and: Filter<Measures, Dimensions>[];
+};
+
+type LogicalOrFilter<Measures extends string, Dimensions extends string> = {
+  or: Filter<Measures, Dimensions>[];
+};
+
+type QueryBuilder<
   Measures extends Keys,
   Dimensions extends Keys,
   InputMeasures extends keyof Measures,
-  InputDimensions extends keyof Dimensions,
+  InputDimensions extends keyof Dimensions
 > = {
   measure<Name extends keyof Measures>(
-    name: Name,
-  ): ResponseBuilder<
-    Measures,
-    Dimensions,
-    InputMeasures | Name,
-    InputDimensions
-  >;
+    name: Name
+  ): QueryBuilder<Measures, Dimensions, InputMeasures | Name, InputDimensions>;
   dimension<Name extends keyof Dimensions>(
-    name: Name,
-  ): ResponseBuilder<
-    Measures,
-    Dimensions,
-    InputMeasures,
-    InputDimensions | Name
-  >;
-  load(api: CubeApi): {
-    [K in InputMeasures]: Measures[K];
-  } & {
-    [K in InputDimensions]: Dimensions[K];
-  };
+    name: Name
+  ): QueryBuilder<Measures, Dimensions, InputMeasures, InputDimensions | Name>;
+  finalize(): { query: Query; parser: () => void };
 };
-
-export function consume<Measures extends Keys, Dimensions extends Keys>(
-  cube: Cube<Measures, Dimensions>,
-): ResponseBuilder<Measures, Dimensions, never, never> {
-  return responseBuilder(cube, [], []);
-}
 
 function responseBuilder<
   Measures extends Keys,
   Dimensions extends Keys,
   InputMeasures extends keyof Measures,
-  InputDimensions extends keyof Dimensions,
+  InputDimensions extends keyof Dimensions
 >(
-  cube: Cube<Measures, Dimensions>,
+  cube: CubeDef<Measures, Dimensions>,
   measures: InputMeasures[],
-  dimensions: InputDimensions[],
-): ResponseBuilder<Measures, Dimensions, InputMeasures, InputDimensions> {
+  dimensions: InputDimensions[]
+): QueryBuilder<Measures, Dimensions, InputMeasures, InputDimensions> {
   return {
     measure: <Name extends keyof Measures>(name: Name) => {
       return responseBuilder(cube, [...measures, name], dimensions);
@@ -65,8 +144,9 @@ function responseBuilder<
     dimension: <Name extends keyof Dimensions>(name: Name) => {
       return responseBuilder(cube, measures, [...dimensions, name]);
     },
-    load: (api) => {
+    finalize: () => {
       const query: Query = {};
+
       if (measures.length) {
         query.measures = measures.map((ms) => `${cube.name}.${String(ms)}`);
       }
@@ -74,22 +154,9 @@ function responseBuilder<
         query.dimensions = dimensions.map((dm) => `${cube.name}.${String(dm)}`);
       }
 
-      return {
-        ...measures.reduce(
-          (acc, curr) => {
-            acc[curr] = cube.measures[curr];
-            return acc;
-          },
-          {} as { [K in InputMeasures]: Measures[K] },
-        ),
-        ...dimensions.reduce(
-          (acc, curr) => {
-            acc[curr] = cube.dimensions[curr];
-            return acc;
-          },
-          {} as { [K in InputDimensions]: Dimensions[K] },
-        ),
-      };
+      query.filters = [{ member: "test", operator: "set" }];
+
+      return { query, parser: () => {} };
     },
   };
 }
@@ -97,21 +164,21 @@ function responseBuilder<
 async function testCube() {
   const api = new CubeApi();
 
-  const cubeDef = elideCube({
-    name: "test",
-    measures: {
+  const cubeDef = new CubeDef(
+    "test",
+    {
       myMeasure: Number,
       myOther: String,
       myThird: Boolean,
     },
-    dimensions: {
+    {
       myDimension: String,
-    },
-  });
+    }
+  );
 
-  const cubeResponse = consume(cubeDef)
+  const cubeResponse = cubeDef
+    .buildQuery()
     .measure("myMeasure")
     .measure("myOther")
-    .dimension("myDimension")
-    .load(api);
+    .dimension("myDimension");
 }
