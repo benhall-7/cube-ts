c19969e45c19e25d354fa3d6927cf7f1e1a5cc43
Benjamin Hall <benjaminjahall@gmail.com>
2025-02-06 13:53:40 -0500

    working types on parse result

--- src/index.ts
+++ src/index.ts
@@ -8,30 +8,56 @@ import {
 type MemberType = "string" | "time" | "boolean" | "number";
 type Member = {
   type: MemberType;
+  parser: (input: unknown) => unknown;
 };
 type Keys = Record<string, Member>;
 
-class CubeDef<Measures extends Keys, Dimensions extends Keys> {
-  name: string;
-  measures: Measures;
-  dimensions: Dimensions;
+class CubeDef<
+  Measures extends Keys,
+  Dimensions extends Keys,
+  Segments extends string
+> {
+  readonly name: string;
+  readonly measures: Measures;
+  readonly dimensions: Dimensions;
+  readonly segments: Segments[];
 
-  constructor(name: string, measures: Measures, dimensions: Dimensions) {
+  constructor(
+    name: string,
+    measures: Measures,
+    dimensions: Dimensions,
+    segments: Segments[]
+  ) {
     this.name = name;
     this.measures = measures;
     this.dimensions = dimensions;
+    this.segments = segments;
   }
 
-  buildQuery(): QueryBuilder<Measures, Dimensions, never, never> {
-    return responseBuilder(this, [], []);
+  buildQuery(): QueryBuilder<Measures, Dimensions, Segments, never, never> {
+    return queryBuilder(this, [], [], []);
   }
 
+  /**
+   * @param name the name of the measure
+   * @returns the "full path" of the cube measure
+   */
   measure<Name extends keyof Measures>(name: Name): string {
-    return `${this.name}.${String(name)}`
+    return `${this.name}.${String(name)}`;
   }
-
+  /**
+   * @param name the name of the dimension
+   * @returns the "full path" of the cube dimension
+   */
   dimension<Name extends keyof Dimensions>(name: Name): string {
-    return `${this.name}.${String(name)}`
+    return `${this.name}.${String(name)}`;
+  }
+  /**
+   * @param name the name of the dimension
+   * @returns the "full path" of the cube dimension
+   */
+  segment(name: Segments): string {
+    return `${this.name}.${name}`;
   }
 }
 
@@ -112,73 +138,157 @@ type LogicalOrFilter<Measures extends string, Dimensions extends string> = {
   or: Filter<Measures, Dimensions>[];
 };
 
-type QueryBuilder<
+type Row<
   Measures extends Keys,
   Dimensions extends Keys,
   InputMeasures extends keyof Measures,
   InputDimensions extends keyof Dimensions
 > = {
-  measure<Name extends keyof Measures>(
-    name: Name
-  ): QueryBuilder<Measures, Dimensions, InputMeasures | Name, InputDimensions>;
-  dimension<Name extends keyof Dimensions>(
-    name: Name
-  ): QueryBuilder<Measures, Dimensions, InputMeasures, InputDimensions | Name>;
-  finalize(): { query: Query; parser: () => void };
+  [K in InputMeasures]: ReturnType<Measures[K]["parser"]>;
+} & {
+  [K in InputDimensions]: ReturnType<Dimensions[K]["parser"]>;
 };
 
-function responseBuilder<
+type QueryBuilder<
   Measures extends Keys,
   Dimensions extends Keys,
+  Segments extends string,
+  InputMeasures extends keyof Measures,
+  InputDimensions extends keyof Dimensions
+> = {
+  measure<Name extends keyof Measures>(
+    name: Name
+  ): QueryBuilder<
+    Measures,
+    Dimensions,
+    Segments,
+    InputMeasures | Name,
+    InputDimensions
+  >;
+  dimension<Name extends keyof Dimensions>(
+    name: Name
+  ): QueryBuilder<
+    Measures,
+    Dimensions,
+    Segments,
+    InputMeasures,
+    InputDimensions | Name
+  >;
+  segment(
+    name: Segments[number]
+  ): QueryBuilder<
+    Measures,
+    Dimensions,
+    Segments,
+    InputMeasures,
+    InputDimensions
+  >;
+  /**
+   * returns a Query object that can be passed to the Cube.js REST endpoint,
+   * and a function to parse individual rows
+   */
+  finalize(): [
+    Query,
+    (
+      row: Record<string, unknown>
+    ) => Row<Measures, Dimensions, InputMeasures, InputDimensions>
+  ];
+};
+
+function queryBuilder<
+  Measures extends Keys,
+  Dimensions extends Keys,
+  Segments extends string,
   InputMeasures extends keyof Measures,
   InputDimensions extends keyof Dimensions
 >(
-  cube: CubeDef<Measures, Dimensions>,
+  cube: CubeDef<Measures, Dimensions, Segments>,
   measures: InputMeasures[],
-  dimensions: InputDimensions[]
-): QueryBuilder<Measures, Dimensions, InputMeasures, InputDimensions> {
+  dimensions: InputDimensions[],
+  segments: Segments[]
+): QueryBuilder<
+  Measures,
+  Dimensions,
+  Segments,
+  InputMeasures,
+  InputDimensions
+> {
   return {
     measure: <Name extends keyof Measures>(name: Name) => {
-      return responseBuilder(cube, [...measures, name], dimensions);
+      return queryBuilder(cube, [...measures, name], dimensions, segments);
     },
     dimension: <Name extends keyof Dimensions>(name: Name) => {
-      return responseBuilder(cube, measures, [...dimensions, name]);
+      return queryBuilder(cube, measures, [...dimensions, name], segments);
+    },
+    segment: (name: Segments) => {
+      return queryBuilder(cube, measures, dimensions, [...segments, name]);
     },
     finalize: () => {
       const query: Query = {};
 
       if (measures.length) {
-        query.measures = measures.map((ms) => `${cube.name}.${String(ms)}`);
+        query.measures = measures.map((ms) => cube.measure(ms));
       }
       if (dimensions.length) {
-        query.dimensions = dimensions.map((dm) => `${cube.name}.${String(dm)}`);
+        query.dimensions = dimensions.map((dm) => cube.dimension(dm));
       }
 
       query.filters = [{ member: "test", operator: "set" }];
 
-      return { query, parser: () => {} };
+      query.segments = ["test"];
+
+      return [
+        query,
+        // row parser
+        (row: Record<string, unknown>) => ({
+          ...measures.reduce((acc, ms) => {
+            const key = cube.measure(ms);
+            const parsed = cube.measures[ms].parser(row[key]) as ReturnType<
+              Measures[InputMeasures]["parser"]
+            >;
+            acc[ms] = parsed;
+            return acc;
+          }, {} as { [K in InputMeasures]: ReturnType<Measures[K]["parser"]> }),
+
+          ...dimensions.reduce((acc, dm) => {
+            const key = cube.dimension(dm);
+            const parsed = cube.dimensions[dm].parser(row[key]) as ReturnType<
+              Dimensions[InputDimensions]["parser"]
+            >;
+            acc[dm] = parsed;
+            return acc;
+          }, {} as { [K in InputDimensions]: ReturnType<Dimensions[K]["parser"]> }),
+        }),
+      ];
     },
   };
 }
 
+const cubeDef = new CubeDef(
+  "test",
+  {
+    myMeasure: { type: "number", parser: Number },
+    myOther: { type: "boolean", parser: Boolean },
+    myThird: { type: "string", parser: String },
+  },
+  {
+    myDimension: { type: "string", parser: String },
+  },
+  ["segment_1", "segment_2"]
+);
+
 async function testCube() {
   const api = new CubeApi();
 
-  const cubeDef = new CubeDef(
-    "test",
-    {
-      myMeasure: Number,
-      myOther: String,
-      myThird: Boolean,
-    },
-    {
-      myDimension: String,
-    }
-  );
-
-  const cubeResponse = cubeDef
+  const [query, rowReader] = cubeDef
     .buildQuery()
     .measure("myMeasure")
     .measure("myOther")
-    .dimension("myDimension");
+    .dimension("myDimension")
+    .finalize();
+
+  const response = await api.load(query);
+  const table = response.tablePivot()[0];
+  const row = rowReader(table);
+  console.log(row.myMeasure, row.myOther, row.myDimension);
 }
